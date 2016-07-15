4c7080e522fa7ef7ea25d96e1763e0a58554e954
root <root@localhost.localdomain>
2016-07-15 11:30:35 +0800

    int a int b

--- Git/src/com/rrx/git/entity/GitTest.java
+++ Git/src/com/rrx/git/entity/GitTest.java
@@ -3,13 +3,13 @@ package com.rrx.git.entity;
 public class GitTest {
 
 	private void mian() {
-
+		int a= 0;
 	}
 
 	public void addTest(){}
 	
 	public static void main(String[] args){
-	
+		int b = 0;	
 	}
 	public static void addTest(){}
 }
