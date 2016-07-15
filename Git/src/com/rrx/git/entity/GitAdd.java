1cdc58faf94b3274ad84686f5d66017203e76a17
licheng <lichenga@jiedaibao.com>
2016-07-15 23:41:25 +0800

    a

--- Git/src/com/rrx/git/entity/GitAdd.java
+++ Git/src/com/rrx/git/entity/GitAdd.java
@@ -4,5 +4,10 @@ public class GitAdd {
 	public static void main(String[] args) {
 		System.out.println();
 	}
-
+	
+<<<<<<< HEAD
+	private int b = 0;
+=======
+	private int a;
+>>>>>>> 47894be2bdf281b4c27895b3e84b4f7c5654df3a
 }
